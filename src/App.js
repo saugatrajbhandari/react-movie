29cb5b251395d25cbabd8b7ecb82b7964646d30e
saugat <saugatshrestha602@gmail.com>
2022-06-20 09:59:57 +0545

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
+import SingleMovie from './SingleMovie'
+import Error from './Error';
+import { BrowserRouter as Router, Routes, Route }  from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<SingleMovie />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+    
   );
 }
 
--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {useParams} from "react-router-dom"
 
 const SingleMovie = () => {
+  const { id } = useParams();
   return (
-    <div>SingleMovie</div>
+    <div> 
+      Our Single movie
+    </div>
   )
 }
 
