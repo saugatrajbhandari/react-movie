a06b070bb88fbf66e26e0fd04efce6d6c5e16de8
saugat <saugatshrestha602@gmail.com>
2022-06-21 16:33:54 +0545

    added context

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AppContext } from './context'
+
 
 const Home = () => {
+  const data = useContext(AppContext)
   return (
-    <h1>Home</h1>
+    <h1>Home {data}</h1>
   )
 }
 
---
+++ src/context.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value={"thapa"}>
+        {children}
+    </AppContext.Provider>
+}
+
+export {AppProvider, AppContext}
