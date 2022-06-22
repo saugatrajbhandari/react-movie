a41ecf25ff9ecb8639155661105d8a1c4f61c367
saugat <saugatshrestha602@gmail.com>
2022-06-22 09:13:31 +0545

    added fetch and state

--- src/Movies.js
+++ src/Movies.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Movies = () => {
+  const {movie} = useGlobalContext();
+  console.log(movie);
   return (
-    <div>Movies</div>
+    <>
+      <h1>Movies</h1>
+      {
+        movie.map((currMovie) => {return(
+          <div>
+            <h1>{currMovie.Title}</h1>
+          </div>
+        )})
+      }
+
+    </>
   )
 }
 
